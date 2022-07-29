588fef1d2ea9500ed081e38c2f6bfee62fd6e68e
Shar1q-codes <shariqhussain691@gmail.com>
2022-07-29 19:28:18 +0530

    done

--- App.js
+++ App.js
@@ -14,23 +14,27 @@ const middleware2 = (req,res,next) => {
 app.use(middleware2)
 app.get('/page1',middleware1, (req,res)=>{
     res.send("For Page 1 both middleware1 and middleware2 are applied");
-    
+    res.end()
 })
 
 app.get('/page2', (req,res)=>{
     res.send("Only middleware2 is applied for Page 2");
+    res.end()
 })
 
 app.get('/page3', (req,res)=>{
     res.send("Only middleware2 is applied for Page 3");
+    res.end()
 })
 
 app.get('/page4', (req,res)=>{
     res.send("Only middleware2 is applied for Page 4");
+    res.end()
 })
 
 app.get('/page5', middleware1, (req,res)=>{
     res.send("For Page 5 both middleware1 and middleware2 are applied");
+    res.end()
 })
 
 app.listen(8080,()=>{
